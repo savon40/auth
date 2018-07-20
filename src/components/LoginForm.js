6bd38ec209a7f1480e9417451ced36ced4591179
Steven Avon <savon@vonage.net>
2018-07-20 14:07:40 -0400

    email and password text inputs working, not connected to firebase tho

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+// import { View, TextInput } from 'react-native';
 
 //import all components from common folder - made possible through common/index.js
-import { Button, Card, CardSection } from './common'; 
+import { Button, Card, CardSection, Input } from './common'; 
+
+/*
+	Path of TextInput:
+		- User types Text
+		- onChageText event called
+		- 'setState' with new text
+		- Component rerenders
+		- when TextInput rerenders, we tell it that its value is 'this.state.text'
+*/
 
 class LoginForm extends Component {
+
+	// add state to component
+	state = {email: '', password: ''};
+
 	render() {
 		return (
 			<Card>
 				<CardSection>
-					<TextInput style={{height: 20, width: 100}} />
+					<Input
+						placeholder="user@gmail.com"
+						autoCorrect={false}
+						label="Email"
+						value ={this.state.email}
+						onChangeText={email => this.setState({email})}
+					/>
 				</CardSection>
 
 				<CardSection>
+					<Input
+						placeholder="password"
+						autoCorrect={false}
+						label="Password"
+						value ={this.state.password}
+						onChangeText={password => this.setState({password})}
+						secureTextEntry
+					/>
 				</CardSection>
 
 				<CardSection>
