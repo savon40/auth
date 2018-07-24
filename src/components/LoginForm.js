790d98a43049a0b5a90a631475f59fd1dcb8f04a
Steven Avon <savon@vonage.net>
2018-07-24 12:40:06 -0400

    added in loading spinner and form clearing after clicking login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,7 +3,7 @@ import { Text } from 'react-native';
 import firebase from 'firebase';
 
 //import all components from common folder - made possible through common/index.js
-import { Button, Card, CardSection, Input } from './common'; 
+import { Button, Card, CardSection, Input, Spinner } from './common'; 
 
 /*
 	Path of TextInput:
@@ -17,23 +17,62 @@ import { Button, Card, CardSection, Input } from './common';
 class LoginForm extends Component {
 
 	// add state to component
-	state = {email: '', password: '', error: ''};
+	state = {email: '', password: '', error: '', loading: false};
 
 	//authenticate user
 	onButtonPress() {
-		const {email, password} = this.state;
-		this.setState({error: ''}); //null out error on re-attempt
 
-		//returns a promise because it takes time to complete request
+		//after button click, we want to show spinner instead of button - use loading state
+
+		const {email, password} = this.state;
+		this.setState({error: '', loading: true}); //null out error on re-attempt //loading true will show spinner
+
+
+		/*
+			returns a promise because it takes time to complete request
+			then() --> what happens when it succeeds
+			catch() --> what happens when it fails
+
+			not sure what .bind is doing at the moment 7-24
+		*/
+
 		firebase.auth().signInWithEmailAndPassword(email,password)
+			.then(this.onLoginSuccess.bind(this))
 			.catch(() => {
 				//catch statement is what happens if it fails - if in here, signin failed
 				firebase.auth().createUserWithEmailAndPassword(email, password)
-					.catch(() => {
-						//failed to create account - show error message
-						this.setState({error: 'Authentication Failed.'});
-					});
+					.then(this.onLoginSuccess.bind(this))
+					.catch(this.onLoginFail.bind(this));
 			});
+	}
+
+	//what we will do when user does login
+	onLoginSuccess() {
+		this.setState({
+			email: '',
+			password: '',
+			error: '',
+			loading: false
+		});	
+	}
+
+	//failed to create account - show error message
+	onLoginFail() {
+		this.setState({error: 'Authentication Failed.', loading: false});	
+	}
+
+	//helper method that returns JSX to show spinner or button tag
+	renderButton() {
+		if (this.state.loading) {
+			return <Spinner size="small"/>;
+		}
+		else {
+			return (
+				<Button onPress={this.onButtonPress.bind(this)}>
+					Login
+				</Button>
+			);
+		}
 	}
 
 	render() {
@@ -63,9 +102,7 @@ class LoginForm extends Component {
 				<Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
 				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>
-						Login
-					</Button>
+					{this.renderButton()}	
 				</CardSection>
 
 			</Card>
